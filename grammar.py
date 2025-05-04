bb788acee92f05f527ffd1483f104252378e7c31
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-04 18:17:46 +1000

    Add more complex grammar

--- grammar.py
+++ grammar.py
@@ -1,37 +1,64 @@
+import datetime
+
 import isodate
 from petitparser import character as c
 
-from models import DayOfWeek
+from models import DayOfWeek, DatePeriod, TimePeriod, ServiceTiming, StopTravelTime
 
 i_separator = c.of('/')
 
-year = (c.digit().times(4) | c.digit().times(2))
-month = c.digit().times(2)
-day = c.digit().times(2)
+year = (c.digit().times(4) | c.digit().times(2)).flatten().map(lambda x: int(x))
+month = c.digit().times(2).flatten().map(lambda x: int(x))
+day = c.digit().times(2).flatten().map(lambda x: int(x))
 d_separator = c.of('-')
 
-date = (year & d_separator & month & d_separator & day)
-date_period = (date & d_separator & date) | date
+date = (year & d_separator & month & d_separator & day).map(lambda x: datetime.date(
+    x[0],
+    month=x[2],
+    day=x[4]
+))
+date_period = ((date & d_separator & date) | date).map(lambda x: DatePeriod.from_array(x))
 
-time_part = c.digit().times(2)
+hour_part = c.digit().repeat(1, 2).flatten().map(lambda x: int(x))
+time_part = c.digit().times(2).flatten().map(lambda x: int(x))
 t_separator = c.of(':')
 
-time = (time_part & t_separator & time_part & (t_separator & time_part).optional())
-time_period = (time & d_separator & time) | time
+time = ((hour_part & t_separator & time_part & (t_separator & time_part).optional())
+        .map(lambda x: datetime.time(
+            hour=x[0],
+            minute=x[2],
+            second=x[4] if len(x) > 4 else 0
+        )))
+time_period = ((time & d_separator & time) | time).map(lambda x: TimePeriod.from_array(x))
 
 day_of_week = ((c.of('1') | c.of('0')).times(7).optional()
-               )
+               .map(lambda x: DayOfWeek.from_array(x)))
 
 duration = ((c.of("P") &
-            (c.digit().repeat(1, 4) & c.of("Y")).optional() &
-            (c.digit().repeat(1, 2) & c.of("M")).optional() &
-            (c.digit().repeat(1, 2) & c.of("D")).optional() &
-            (c.of("T") &
-             (c.digit().repeat(1, 2) & c.of("H")).optional() &
+             (c.digit().repeat(1, 4) & c.of("Y")).optional() &
              (c.digit().repeat(1, 2) & c.of("M")).optional() &
-             (c.digit().repeat(1, 2) & c.of("S")).optional()).optional())
-            ).flatten().map(lambda x: isodate.parse_duration(x))
+             (c.digit().repeat(1, 2) & c.of("D")).optional() &
+             (c.of("T") &
+              (c.digit().repeat(1, 2) & c.of("H")).optional() &
+              (c.digit().repeat(1, 2) & c.of("M")).optional() &
+              (c.digit().repeat(1, 2) & c.of("S")).optional()).optional())
+).flatten().map(lambda x: isodate.parse_duration(x))
 
-event_time_definition = (date_period & i_separator & day_of_week.optional() & i_separator & time_period & i_separator & duration)
+event_time_definition = (
+        date_period & i_separator &
+        day_of_week & i_separator &
+        time_period & i_separator &
+        duration
+).map(lambda x: ServiceTiming(
+    x[0],
+    x[4],
+    x[2],
+    x[6]
+))
 
-print(event_time_definition.parse("2025-05-10-2025-05-12/1011111/10:12-12:12/PT20M").value)
+stop_id = (c.digit() | c.letter()).plus().flatten()
+stop_travel_time = (stop_id & (c.of(":") & duration).optional()).map(lambda x: StopTravelTime.from_array(x))
+stop_sequence = (stop_travel_time.separated_by(c.of(","))).plus().map(lambda x: x[0][0::2])
+
+print(event_time_definition.parse("2025-05-10-2025-05-12//10:12-13:12/PT20M").value)
+print(stop_sequence.parse("asd122:PT12H,rffr2212,rffr2212"))
--- models.py
+++ models.py
@@ -1,4 +1,9 @@
+import datetime
 from dataclasses import dataclass
+from time import strftime
+from typing import Optional
+
+from isodate import Duration
 
 
 @dataclass
@@ -14,7 +19,7 @@ class DayOfWeek:
     @staticmethod
     def from_array(x) -> "DayOfWeek":
         if x:
-            DayOfWeek(
+            return DayOfWeek(
                 x[0] == "1",
                 x[1] == "1",
                 x[2] == "1",
@@ -24,7 +29,7 @@ class DayOfWeek:
                 x[6] == "1",
             )
         else:
-            DayOfWeek(
+            return DayOfWeek(
                 True,
                 True,
                 True,
@@ -33,3 +38,58 @@ class DayOfWeek:
                 True,
                 True,
             )
+
+
+@dataclass
+class DatePeriod:
+    start: datetime.date
+    end: Optional[datetime.date]
+
+    @staticmethod
+    def from_array(x) -> "DatePeriod":
+        if len(x) == 1:
+            return DatePeriod(x[0], None)
+        else:
+            return DatePeriod(x[0], x[2])
+
+
+@dataclass
+class TimePeriod:
+    start: datetime.time
+    end: Optional[datetime.time]
+
+    @staticmethod
+    def from_array(x) -> "TimePeriod":
+        if len(x) == 1:
+            return TimePeriod(x[0], None)
+        else:
+            return TimePeriod(x[0], x[2])
+
+
+@dataclass
+class ServiceTiming:
+    datePeriod: DatePeriod
+    timePeriod: TimePeriod
+    dayOfWeek: DayOfWeek
+    repetitionDuration: Duration
+
+    def id(self, route_id: str) -> str:
+        elements = [
+            self.datePeriod.start.strftime('%Y-%m-%d'),
+            self.datePeriod.end.strftime('%Y-%m-%d') if self.datePeriod.end else None,
+            route_id
+        ]
+        return '-'.join(list(filter(lambda x: x is not None, elements)))
+
+
+@dataclass
+class StopTravelTime:
+    stop_id: str
+    travel_time: Optional[Duration]
+
+    @staticmethod
+    def from_array(x) -> "StopTravelTime":
+        if x[1] is None:
+            return StopTravelTime(x[0], None)
+        else:
+            return StopTravelTime(x[0], x[1][1])
